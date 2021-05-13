1228ca6262c2e125327bfd65cdc679ca1bc48722
NathanScolaire <1837576@cstj.qc.ca>
2021-05-12 20:27:27 -0400

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Pog
+          Modification de type oui
         </p>
         <a
           className="App-link"
